2b83c89163d66715e91e55f0d0c41b614b604625
Thiflan <thiflan120699@gmail.com>
2023-07-28 18:47:17 +1200

    additionals

--- shorthands.js
+++ shorthands.js
@@ -63,4 +63,65 @@ Math.pow(2, 2);
 2 ** 3;
 2 ** 2;
 
+/* Joining and Cloning arrays */
 
+// Longhand
+
+//Joining arrays
+const odd = [1, 3, 5];
+const even = [2, 4, 6].concat(odd);
+console.log(even);
+
+//Cloning arrays
+const arr1 = [1, 2, 3, 4];
+const arr2 = arr1.slice();
+
+// Shorthand
+
+//Joining arrays
+const odd = [1, 3, 5];
+const even = [2, 4, 6, ...odd];
+console.log(even);
+
+//Cloning arrays
+const arr1 = [1, 2, 3, 4];
+const arr2 = [...arr1];
+
+/* String Into A Number */
+
+// Longhand
+const num1 = parseInt("100");
+const num2 = parseFloat("100.01");
+
+// Shorthand
+const num1 = +"100";
+const num2 = +"100.01";
+
+/* Destructuring Assignment */
+
+// Longhand
+const observable = require("mobx/observable");
+const action = require("mobx/action");
+const runInAction = require("mobx/runInAction");
+
+const store = this.props.store;
+const form = this.props.form;
+const loading = this.props.loading;
+const errors = this.props.errors;
+const entity = this.props.entity;
+
+// Shorthand
+import { observable, action, runInAction } from "mobx";
+const { store, form, loading, errors, entity } = this.props;
+// You can even assign variable names
+const { store, form, loading, errors, entity: contact } = this.props;
+
+/* Template Literals */
+
+//Longhand
+const welcome = "You have logged in as " + first + " " + last + ".";
+const db = "http://" + host + ":" + port + "/" + database;
+
+// Shorthand
+const welcome = `You have logged in as ${first} ${last}`;
+const db = `http://${host}:${port}/${database}`;
